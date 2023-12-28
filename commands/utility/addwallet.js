27a1a4765daad6723fc75f9f97264dbc04983f70
rookmate <14072042+rookmate@users.noreply.github.com>
2023-12-28 19:14:42 +0000

    Add input sanitization

--- commands/utility/addwallet.js
+++ commands/utility/addwallet.js
@@ -1,5 +1,17 @@
 const { SlashCommandBuilder } = require('discord.js');
 const wait = require('node:timers/promises').setTimeout;
+const Web3 = require('web3');
+
+function isValidEthereumAddress(address) {
+    // Check if the address matches the Ethereum address format
+    const addressRegex = /^(0x)?[0-9a-fA-F]{40}$/;
+    if (!addressRegex.test(address)) {
+        return false;
+    }
+
+    // Check if the address is a valid checksum address
+    return Web3.utils.isAddress(address);
+}
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -13,6 +25,11 @@ module.exports = {
 
     async execute(interaction) {
         const walletAddress = interaction.options.getString('address');
+        if (!isValidEthereumAddress(walletAddress)) {
+            await interaction.reply({content: `${walletAddress} is not a valid Ethereum address`, ephemeral: true});
+            return;
+        }
+
         await interaction.reply({content: `Checking if user has role permissions`, ephemeral: true});
         await wait(1000);
         await interaction.followUp({content: `Checking if wallet provided contains the allowed NFTs`, ephemeral: true});
