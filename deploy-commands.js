b40673f84878e794f34858067316a8a4158befac
rookmate <14072042+rookmate@users.noreply.github.com>
2023-12-28 22:31:04 +0000

    Add constants file

--- deploy-commands.js
+++ deploy-commands.js
@@ -34,7 +34,7 @@ const rest = new REST().setToken(process.env.DISCORD_TOKEN);
 
 		// The put method is used to fully refresh all commands in the guild with the current set
 		const data = await rest.put(
-			Routes.applicationGuildCommands(process.env.APP_ID, process.env.SERVER_ID),
+			Routes.applicationGuildCommands(process.env.APP_ID, process.env.GUILD_ID),
 			{ body: commands },
 		);
 
