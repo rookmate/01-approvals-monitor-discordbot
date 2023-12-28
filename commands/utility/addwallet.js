4ec93164b6ad1c5163a910c09102cfe980d40e2e
rookmate <14072042+rookmate@users.noreply.github.com>
2023-12-28 22:31:42 +0000

    Add input sanitization and role check

--- commands/utility/addwallet.js
+++ commands/utility/addwallet.js
@@ -1,4 +1,5 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, PermissionsBitField } = require('discord.js');
+const { ALLOWED_ROLES } = require("../../constants.js");
 const wait = require('node:timers/promises').setTimeout;
 const Web3 = require('web3');
 
@@ -24,18 +25,28 @@ module.exports = {
                 .setRequired(true)),
 
     async execute(interaction) {
+        console.log(`Checking if user has role permissions`);
+        const matchingRoles = Object.entries(ALLOWED_ROLES).filter(([key, value]) =>
+            Array.from(interaction.member.roles.cache.keys()).includes(value)
+        );
+        console.log(matchingRoles);
+        if (matchingRoles.length > 0) {
+            await interaction.reply({content: `You do not have permissions to run this command`, ephemeral: true});
+            return;
+        }
+
+        console.log(`Sanitizing input values`);
         const walletAddress = interaction.options.getString('address');
         if (!isValidEthereumAddress(walletAddress)) {
             await interaction.reply({content: `${walletAddress} is not a valid Ethereum address`, ephemeral: true});
             return;
         }
 
-        await interaction.reply({content: `Checking if user has role permissions`, ephemeral: true});
-        await wait(1000);
-        await interaction.followUp({content: `Checking if wallet provided contains the allowed NFTs`, ephemeral: true});
-        await wait(1000);
-        await interaction.followUp({content: `Adding wallet to alert system`, ephemeral: true});
-        await wait(1000);
-        await interaction.followUp({content: `Successfully added wallet ${walletAddress}`, ephemeral: true});
+        //await wait(1000);
+        //await interaction.followUp({content: `Checking if wallet provided contains the allowed NFTs`, ephemeral: true});
+        //await wait(1000);
+        //await interaction.followUp({content: `Adding wallet to alert system`, ephemeral: true});
+        //await wait(1000);
+        await interaction.reply({content: `Successfully added wallet ${walletAddress}`, ephemeral: true});
     },
 };
