9b38fa2abcc31553c60c85986c99abeb4af0fed4
rookmate <14072042+rookmate@users.noreply.github.com>
2023-12-31 12:30:09 +0000

    Add validation that wallet owns NFTs

--- commands/utility/addwallet.js
+++ commands/utility/addwallet.js
@@ -1,7 +1,9 @@
-const { SlashCommandBuilder, PermissionsBitField } = require('discord.js');
-const { ALLOWED_ROLES } = require("../../constants.js");
-const wait = require('node:timers/promises').setTimeout;
-const Web3 = require('web3');
+const { SlashCommandBuilder } = require('discord.js');
+const { ALLOWED_ROLES, ALLOWED_NFTS } = require("../../constants.js");
+require('dotenv').config();
+const { isAddress, createPublicClient, http, recoverMessageAddress, isAddressEqual } = require('viem');
+const { mainnet, polygon } = require('viem/chains');
+const fs = require("fs");
 
 function isValidEthereumAddress(address) {
     // Check if the address matches the Ethereum address format
@@ -11,42 +13,82 @@ function isValidEthereumAddress(address) {
     }
 
     // Check if the address is a valid checksum address
-    return Web3.utils.isAddress(address);
+    return isAddress(address);
 }
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('addwallet')
-        .setDescription('Allows the user to add a wallet for the bot to alert open approvals')
+        .setDescription('Allows the user to add a wallet that is suitable for the bot to alert open approvals')
         .addStringOption(option =>
             option
                 .setName('address')
                 .setDescription('Wallet address for the bot to monitor')
-                .setRequired(true)),
+                .setRequired(true)
+        )
+        .addStringOption(option =>
+            option
+                .setName('signature')
+                .setDescription('Signature proving wallet is theirs')
+        ),
 
     async execute(interaction) {
         console.log(`Checking if user has role permissions`);
         const matchingRoles = Object.entries(ALLOWED_ROLES).filter(([key, value]) =>
             Array.from(interaction.member.roles.cache.keys()).includes(value)
         );
-        console.log(matchingRoles);
-        if (matchingRoles.length > 0) {
+        if (matchingRoles.length === 0) {
             await interaction.reply({content: `You do not have permissions to run this command`, ephemeral: true});
             return;
         }
 
-        console.log(`Sanitizing input values`);
-        const walletAddress = interaction.options.getString('address');
-        if (!isValidEthereumAddress(walletAddress)) {
-            await interaction.reply({content: `${walletAddress} is not a valid Ethereum address`, ephemeral: true});
+        console.log(`Validating if valid wallet address`);
+        const userAddress = interaction.options.getString('address');
+        if (!isValidEthereumAddress(userAddress)) {
+            await interaction.reply({content: `${userAddress} is not a valid Ethereum address`, ephemeral: true});
             return;
         }
 
-        //await wait(1000);
-        //await interaction.followUp({content: `Checking if wallet provided contains the allowed NFTs`, ephemeral: true});
-        //await wait(1000);
-        //await interaction.followUp({content: `Adding wallet to alert system`, ephemeral: true});
-        //await wait(1000);
-        await interaction.reply({content: `Successfully added wallet ${walletAddress}`, ephemeral: true});
+        // TODO: ADD VALIDATION IF WALLET ALREADY EXISTS ON THE DATABASE AND RETURN IF IT DOES
+        console.log(`Checking if wallet provided contains the allowed NFTs`);
+        let userOwnedNFTs = { "total": 0n };
+        for (const nft of ALLOWED_NFTS) {
+            let _clientChainData;
+            if (nft.chain === "ethereum") {
+                _clientChainData = { chain: mainnet, transport: http(`https://eth-mainnet.g.alchemy.com/v2/${process.env.ETH_ALCHEMY_KEY}`), };
+            }
+
+            if (nft.chain === "polygon"){
+                _clientChainData = { chain: polygon, transport: http(`https://polygon-mainnet.g.alchemy.com/v2/${process.env.POLYGON_ALCHEMY_KEY}`), };
+            }
+
+            const client = createPublicClient(_clientChainData)
+            try {
+                const balance = await client.readContract({ ...nft, functionName: 'balanceOf', args: [userAddress, nft.tokenID], });
+                userOwnedNFTs[nft.name] = balance;
+                userOwnedNFTs["total"] += balance;
+            } catch (error) {
+                await interaction.reply({content: `Could not retrieve data from RPC. Please reach out to a moderator.`, ephemeral: true});
+                console.error('Error:', error);
+                return;
+            }
+        }
+
+        const messageToSign = `Verification for NFT ownership for wallet address ${userAddress}.`;
+        const userSig = interaction.options.getString('signature');
+        if (userSig === null) {
+            console.log(`Address ${userAddress} owns ${userOwnedNFTs["total"]} NFTs. ${interaction.user.username} needs to prove it owns the NFTs in the address.`);
+            interaction.reply({ content: `Please sign the following message with your Ethereum private key:\n\n\`${messageToSign}\`\n\nRun this command again filling in the \`signature\` field`, ephemeral: true});
+        }
+        else {
+            console.log(`Sig provided continue dev`);
+            const recoveredAddress = await recoverMessageAddress({ message: messageToSign, userSig });
+            if (isAddressEqual(recoveredAddress, userAddress)) {
+                //TODO: console.log(`Adding wallet to alert system`);
+                await interaction.reply({ content: `Successfully added wallet ${userAddress}`, ephemeral: true});
+            } else {
+                interaction.reply({ content: 'Error verifying NFT ownership.', ephemeral: true});
+            }
+        }
     },
 };
